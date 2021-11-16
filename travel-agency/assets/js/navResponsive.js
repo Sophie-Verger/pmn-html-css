f7bb0a3363ee64c3334e92bb4591888424d36c8d
Sophie Verger <sophie.verger2@gmail.com>
2021-11-16 17:13:12 +0100

    initial commit with travel agency index finished

---
+++ travel-agency/assets/js/navResponsive.js
@@ -0,0 +1,10 @@
+function navBar() {
+  var x = document.getElementsByClassName("navBar");
+  if (x[0].style.display === "none") {
+    x[0].style.display = "block";
+  } else {
+    x[0].style.display = "none";
+  }
+
+}
+
